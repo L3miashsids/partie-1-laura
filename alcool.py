fd23c22493fadfa940ecb7132f313fdb0790b5e4
L3miashsids <76964955+L3miashsids@users.noreply.github.com>
2021-01-10 17:51:03 +0100

    Add files via upload

---
+++ alcool.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 30 14:26:39 2020
+
+@author: laura
+"""
+
+#Importations
+import os
+import pandas as pd
+import numpy as np
+import math as m
+import fonctions as f
+
+
+#Chargement du dossier
+os.chdir("C:/Users/Utilisateur/Documents/L3 MIASHS/Python/python Laura")
+
+#Chargement premiere feuille --- Données TRAIN
+DT = pd.read_excel("Data_LDA_Python.xlsx",sheet_name="DATA_TRAIN")
+
+import numpy as np
+
+Classe=f.ADL()
+
+#FIT
+Classe.fit(DT, "TYPE")
+
+#Chargement seconde feuille --- Données PREDICT
+DP = pd.read_excel("Data_LDA_Python.xlsx",sheet_name="DATA_PREDICT")
+
+#PREDICT
+
+
+'''
+stepdisc = f.stepdisc(???)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
