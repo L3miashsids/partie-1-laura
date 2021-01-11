e83ee92face1c69039528d900555a4a89f8c24a6
L3miashsids <76964955+L3miashsids@users.noreply.github.com>
2021-01-11 19:49:56 +0100

    Update programme_principal.py

--- programme_principal.py
+++ programme_principal.py
@@ -1,56 +1,34 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec 30 14:26:39 2020
-
 @author: laura
 """
 
 #Importations
 import os
 import pandas as pd
-import numpy as np
-import math as m
-import fonctions as f
+import fonctions4 as f
 
 
 #Chargement du dossier
-os.chdir("C:/Users/Utilisateur/Documents/L3 MIASHS/Python/python Laura")
+os.chdir("C:/Users/laura/OneDrive/Bureau/PYTHON")
 
 #Chargement premiere feuille --- Données TRAIN
 DT = pd.read_excel("Data_LDA_Python.xlsx",sheet_name="DATA_TRAIN")
 
-import numpy as np
-
 Classe=f.ADL()
 
 #FIT
-fit= Classe.fit(DT, "TYPE")
+fit = Classe.fit(DT, "TYPE")
 
 #Chargement seconde feuille --- Données PREDICT
 DP = pd.read_excel("Data_LDA_Python.xlsx",sheet_name="DATA_PREDICT")
 
 #PREDICT
-predict = f.predict(fit, DP)
+predict = Classe.predict(fit, DP)
 
-'''
-stepdisc = f.stepdisc(???)
-'''
+#STEPDISC
+Classe.stepdisc(DT, "TYPE")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#HTML
+htlm = f.HTML()
